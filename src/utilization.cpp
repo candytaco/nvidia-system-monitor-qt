dc579ac8443f407e9c09e3730c2bb84b771b31bc
tzhang <candytaco@gmail.com>
2019-11-12 13:36:23 -0800

    formatting fix, display MB use in vRAM

--- src/utilization.cpp
+++ src/utilization.cpp
@@ -11,252 +11,277 @@
 #define graphHeightCoef 9
 int grapthStartY, grapthEndY, width;
 
-void drawGrid(QWidget *widget, QPainter *p, const char *name) {
-    QFontMetrics fm(qApp->font());
-    int x0, y0 = fm.height(), graphHeight = graphHeightCoef * y0;
-    width = widget->size().width() - 4;
-    
-    p->setPen(QColor(100, 100, 100));
-    grapthStartY = y0 * 1.25f;
-    grapthEndY = grapthStartY + graphHeight;
-    for (float i = 0; i <= 1.0f; i += 0.25f) {
-        p->drawLine(width * i, grapthStartY, width * i, grapthStartY + graphHeight);
-        p->drawLine(0, grapthStartY + graphHeight * i, width, grapthStartY + graphHeight * i);
-    }
-    
-    p->setPen(QApplication::palette().text().color());
-    p->drawText(0, y0, (name + toString(UPDATE_DELAY / 1000.0f) + " sec step").c_str());
-    p->drawText(0, grapthEndY + y0, (toString(GRAPH_LENGTH / 1000.0f) + " sec").c_str());
-    
-    QString text = "100%";
-    x0 = fm.horizontalAdvance(text);
-    p->drawText(width - x0, y0, text);
-    
-    text = "0%";
-    x0 = fm.horizontalAdvance(text);
-    p->drawText(width - x0, grapthEndY + y0, text);
+void drawGrid(QWidget* widget, QPainter* p, const char* name)
+{
+	QFontMetrics fm(qApp->font());
+	int x0, y0 = fm.height(), graphHeight = graphHeightCoef * y0;
+	width = widget->size().width() - 4;
+
+	p->setPen(QColor(100, 100, 100));
+	grapthStartY = y0 * 1.25f;
+	grapthEndY = grapthStartY + graphHeight;
+	for (float i = 0; i <= 1.0f; i += 0.25f)
+	{
+		p->drawLine(width * i, grapthStartY, width * i, grapthStartY + graphHeight);
+		p->drawLine(0, grapthStartY + graphHeight * i, width, grapthStartY + graphHeight * i);
+	}
+
+	p->setPen(QApplication::palette().text().color());
+	p->drawText(0, y0, name);
+	p->drawText(0, grapthEndY + y0, (toString(GRAPH_LENGTH / 1000.0f) + " sec").c_str());
+
+	QString text = "100%";
+	x0 = fm.horizontalAdvance(text);
+	p->drawText(width - x0, y0, text);
+
+	text = "0%";
+	x0 = fm.horizontalAdvance(text);
+	p->drawText(width - x0, grapthEndY + y0, text);
 }
 
-void drawGraph(UtilizationWorker *worker, QPainter *p) {
-    int x1, y1, x2, y2;
-    QColor color;
-    QPen pen;
-    pen.setWidth(2);
-    for (int g = 0; g < GPU_COUNT; g++) {
-        color = gpuColors[g];
-        pen.setColor(color);
-        p->setPen(pen);
-        for (size_t i = 1; i < worker->gpoints[g].size(); i++) {
-            x1 = worker->gpoints[g][i - 1].x * width;
-            y1 = grapthEndY - (grapthEndY - grapthStartY) / 100.0f * worker->gpoints[g][i - 1].y;
-            x2 = worker->gpoints[g][i].x * width;
-            y2 = grapthEndY - (grapthEndY - grapthStartY) / 100.0f * worker->gpoints[g][i].y;
-                
-            QPainterPath filling;
-            filling.moveTo(x1, y1);
-            filling.lineTo(x2, y2);
-            filling.lineTo(x2, grapthEndY);
-            filling.lineTo(x1, grapthEndY);
-            filling.lineTo(x1, y1);
-            color.setAlpha(64);
-            p->fillPath(filling, QBrush(color));
-                
-            p->drawLine(x1, y1, x2, y2);
-        }
-    }
+void drawGraph(UtilizationWorker* worker, QPainter* p)
+{
+	int x1, y1, x2, y2;
+	QColor color;
+	QPen pen;
+	pen.setWidth(2);
+	for (int g = 0; g < GPU_COUNT; g++)
+	{
+		color = gpuColors[g];
+		pen.setColor(color);
+		p->setPen(pen);
+		for (size_t i = 1; i < worker->graphPoints[g].size(); i++)
+		{
+			x1 = worker->graphPoints[g][i - 1].x * width;
+			y1 = grapthEndY - (grapthEndY - grapthStartY) / 100.0f * worker->graphPoints[g][i - 1].y;
+			x2 = worker->graphPoints[g][i].x * width;
+			y2 = grapthEndY - (grapthEndY - grapthStartY) / 100.0f * worker->graphPoints[g][i].y;
+
+			QPainterPath filling;
+			filling.moveTo(x1, y1);
+			filling.lineTo(x2, y2);
+			filling.lineTo(x2, grapthEndY);
+			filling.lineTo(x1, grapthEndY);
+			filling.lineTo(x1, y1);
+			color.setAlpha(64);
+			p->fillPath(filling, QBrush(color));
+
+			p->drawLine(x1, y1, x2, y2);
+		}
+	}
 }
 
-void drawStatusObjects(std::vector<QRect> &statusObjectsAreas, UtilizationData *udata, QPainter* p) {
-    statusObjectsAreas.clear();
-    QFontMetrics fm(qApp->font());
-    int size = fm.height() * 2; // width and height for progress arc
-    int textWidth = fm.horizontalAdvance("100%"); // because max width will be only at 100%
-    
-    int blockSize = size + STATUS_OBJECT_TEXT_OFFSET + textWidth + STATUS_OBJECT_OFFSET;
-    int horizontalCount = (width + STATUS_OBJECT_OFFSET) / blockSize; // (width + STATUS_OBJECT_OFFSET) because last element has offset
-    int spanAngle, x, y;
-    QRect progress;
-    
-    for (int g = 0; g < GPU_COUNT; g++) {
-        p->setPen(gpuColors[g]);
-        p->setBrush(QBrush(gpuColors[g]));
-        
-        x = blockSize * (g % horizontalCount);
-        y = grapthEndY + fm.height() + (size + STATUS_OBJECT_OFFSET) * (g / horizontalCount) + GRAPTH_OFFSET;
-        spanAngle = -udata[g].level / 100.0f * 360;
-        
-        progress = QRect(x, y, size, size);
-        
-        QPainterPath progressPath;
-        progressPath.moveTo(x + size / 2, y + size / 2);
-        progressPath.arcTo(progress, 90, spanAngle);
-        p->drawPath(progressPath);
-        
-        p->setPen(QApplication::palette().text().color());
-        p->setBrush(QBrush());
-        p->drawEllipse(x, y, size, size);
-        
-        p->drawText(x + size + STATUS_OBJECT_TEXT_OFFSET, y + size / 2 + fm.xHeight() / 2, (std::to_string(udata[g].level) + "%").c_str());
-        
-        statusObjectsAreas.emplace_back(x, y, blockSize, size);
-    }
+void drawStatusObjects(std::vector<QRect>& statusObjectsAreas, UtilizationData* utilizationData, QPainter* p)
+{
+	statusObjectsAreas.clear();
+	QFontMetrics fontMetric(qApp->font());
+	int size = fontMetric.height() * 2; 					// width and height for progress arc
+	int spanAngle, x, y, textWidth;
+	int blockSize, horizontalCount;
+	QRect progress;
+
+	for (int GPU = 0; GPU < GPU_COUNT; GPU++)
+	{
+
+		if (utilizationData[GPU].maximum == 100.0)
+			textWidth = fontMetric.horizontalAdvance("100%");	// calc max width
+		else
+			textWidth = fontMetric.horizontalAdvance("00000 / 00000 MB");
+		blockSize = size + STATUS_OBJECT_TEXT_OFFSET + textWidth + STATUS_OBJECT_OFFSET;
+		horizontalCount = (width + STATUS_OBJECT_OFFSET) / blockSize; // (width + STATUS_OBJECT_OFFSET) because last element has offset
+
+		p->setPen(gpuColors[GPU]);
+		p->setBrush(QBrush(gpuColors[GPU]));
+
+		x = blockSize * (GPU % horizontalCount);
+		y = grapthEndY + fontMetric.height() + (size + STATUS_OBJECT_OFFSET) * (GPU / horizontalCount) + GRAPTH_OFFSET;
+		spanAngle = -utilizationData[GPU].level / utilizationData[GPU].maximum * 360;
+
+		progress = QRect(x, y, size, size);
+
+		QPainterPath progressPath;
+		progressPath.moveTo(x + size / 2, y + size / 2);
+		progressPath.arcTo(progress, 90, spanAngle);
+		p->drawPath(progressPath);
+
+		p->setPen(QApplication::palette().text().color());
+		p->setBrush(QBrush());
+		p->drawEllipse(x, y, size, size);
+
+		if (utilizationData[GPU].maximum == 100.0f)
+			p->drawText(x + size + STATUS_OBJECT_TEXT_OFFSET, y + size / 2 + fontMetric.xHeight() / 2,
+						(std::to_string(utilizationData[GPU].level) + "%").c_str());
+		else
+			p->drawText(x + size + STATUS_OBJECT_TEXT_OFFSET, y + size / 2 + fontMetric.xHeight() / 2,
+						(std::to_string(utilizationData[GPU].level) + " / " + std::to_string(int(utilizationData[GPU].maximum)) + " MB").c_str());
+
+		statusObjectsAreas.emplace_back(x, y, blockSize, size);
+	}
 }
 
-Point::Point(const float x, const int y) {
-    this->x = x;
-    this->y = y;
+Point::Point(const float x, const int y)
+{
+	this->x = x;
+	this->y = y;
 }
 
-UtilizationWorker::UtilizationWorker() {
-    gpoints = new std::vector<Point>[GPU_COUNT];
-    udata = new UtilizationData[GPU_COUNT];
+UtilizationWorker::UtilizationWorker()
+{
+	graphPoints = new std::vector<Point>[GPU_COUNT];
+	utilizationData = new UtilizationData[GPU_COUNT];
 }
 
-void UtilizationWorker::work() {
-    mutex.lock();
+void UtilizationWorker::work()
+{
+	mutex.lock();
 
-    if (lastTime == 0) {
-        lastTime = getTime();
-        mutex.unlock();
-        return;
-    }
-    
-    receiveData();
-    
-    float step = (float)(getTime() - lastTime) / UPDATE_DELAY * GRAPH_STEP;
-    
-    // g means gpu
-    for (int g = 0; g < GPU_COUNT; g++) {
-        for (Point &i : gpoints[g])
-            i.x -= step;
-        
-        gpoints[g].emplace_back(1.0f, udata[g].level);
-        deleteSuperfluousPoints(g);
+	if (lastTime == 0)
+	{
+		lastTime = getTime();
+		mutex.unlock();
+		return;
+	}
 
-        // calculate average, min, max
-        udata[g].avgLevel = udata[g].maxLevel = 0;
-        udata[g].minLevel = 100;
-        for (size_t i = 0; i < gpoints[g].size(); i++) {
-            udata[g].avgLevel += gpoints[g][i].y;
-            if (udata[g].maxLevel < gpoints[g][i].y)
-                udata[g].maxLevel = gpoints[g][i].y;
-            if (udata[g].minLevel > gpoints[g][i].y)
-                udata[g].minLevel = gpoints[g][i].y;
-        }
-        udata[g].avgLevel /= gpoints[g].size();
-    }
-    
-    mutex.unlock();
-    dataUpdated();
-    
-    lastTime = getTime();
+	receiveData();
+
+	float step = (float)(getTime() - lastTime) / UPDATE_DELAY * GRAPH_STEP;
+
+	for (int GPU = 0; GPU < GPU_COUNT; GPU++)
+	{
+		for (Point& i : graphPoints[GPU])
+			i.x -= step;
+
+		graphPoints[GPU].emplace_back(1.0f, utilizationData[GPU].level * 100 / utilizationData[GPU].maximum);
+		deleteSuperfluousPoints(GPU);
+
+		// calculate average, min, max
+		utilizationData[GPU].avgLevel = utilizationData[GPU].maxLevel = 0;
+		utilizationData[GPU].minLevel = 100;
+		for (size_t i = 0; i < graphPoints[GPU].size(); i++)
+		{
+			utilizationData[GPU].avgLevel += graphPoints[GPU][i].y;
+			if (utilizationData[GPU].maxLevel < graphPoints[GPU][i].y)
+				utilizationData[GPU].maxLevel = graphPoints[GPU][i].y;
+			if (utilizationData[GPU].minLevel > graphPoints[GPU][i].y)
+				utilizationData[GPU].minLevel = graphPoints[GPU][i].y;
+		}
+		utilizationData[GPU].avgLevel /= graphPoints[GPU].size();
+	}
+
+	mutex.unlock();
+	dataUpdated();
+
+	lastTime = getTime();
 }
 
-void UtilizationWorker::deleteSuperfluousPoints(const uint index) {
-    if (gpoints[index].size() > 2 && gpoints[index][0].x < 0 && gpoints[index][1].x <= 0)
-        gpoints[index].erase(gpoints[index].begin());
+void UtilizationWorker::deleteSuperfluousPoints(const uint index)
+{
+	if (graphPoints[index].size() > 2 && graphPoints[index][0].x < 0 && graphPoints[index][1].x <= 0)
+		graphPoints[index].erase(graphPoints[index].begin());
 }
 
-UtilizationWorker::~UtilizationWorker() {
-    delete[] gpoints;
-    delete[] udata;
+UtilizationWorker::~UtilizationWorker()
+{
+	delete[] graphPoints;
+	delete[] utilizationData;
 }
 
-void GPUUtilizationWorker::receiveData() {
-    std::vector<std::string> lines = split(exec(NVSMI_CMD_GPU_UTILIZATION), "\n");
-    // fake, "emulated" GPUs
+void GPUUtilizationWorker::receiveData()
+{
+	std::vector<std::string> lines = split(exec(NVSMI_CMD_GPU_UTILIZATION), "\n");
+	// fake, "emulated" GPUs
 //    lines[2] = std::to_string(50 + rand() % 20) + " %";
 //    lines.push_back(std::to_string(30 + rand() % 10) + " %");
 //    lines.push_back(std::to_string(70 + rand() % 30) + " %");
 //    lines.push_back("");
-    for (size_t i = 1; i < lines.size() - 1; i++)
-        udata[i - 1].level = std::atoi(split(lines[i], " ")[0].c_str());
+	for (size_t i = 1; i < lines.size() - 1; i++)
+		utilizationData[i - 1].level = std::atoi(split(lines[i], " ")[0].c_str());
 }
 
-MemoryUtilizationWorker::MemoryUtilizationWorker() : UtilizationWorker() {
-    memoryData = new MemoryData[GPU_COUNT];
+MemoryUtilizationWorker::MemoryUtilizationWorker() : UtilizationWorker()
+{
+	memoryData = new MemoryData[GPU_COUNT];
 }
 
-MemoryUtilizationWorker::~MemoryUtilizationWorker() {
-    delete[] memoryData;
+MemoryUtilizationWorker::~MemoryUtilizationWorker()
+{
+	delete[] memoryData;
 }
 
-void MemoryUtilizationWorker::receiveData() {
-    std::vector<std::string> lines = split(exec(NVSMI_CMD_MEM_UTILIZATION), "\n"), data;
-    for (size_t i = 1; i < lines.size() - 1; i++) {
-        data = split(lines[i], ", ");
-        memoryData[i - 1].total = std::atoi(split(data[1], " ")[0].c_str());
-        memoryData[i - 1].free = std::atoi(split(data[2], " ")[0].c_str());
-        memoryData[i - 1].used = std::atoi(split(data[3], " ")[0].c_str());
-        udata[i - 1].level = memoryData[i - 1].used * 100 / memoryData[i - 1].total;
-    }
+void MemoryUtilizationWorker::receiveData()
+{
+	std::vector<std::string> lines = split(exec(NVSMI_CMD_MEM_UTILIZATION), "\n"), data;
+	for (size_t GPU = 1; GPU < lines.size() - 1; GPU++)
+	{
+		data = split(lines[GPU], ", ");
+		memoryData[GPU - 1].total = std::atoi(split(data[1], " ")[0].c_str());
+		memoryData[GPU - 1].free = std::atoi(split(data[2], " ")[0].c_str());
+		memoryData[GPU - 1].used = std::atoi(split(data[3], " ")[0].c_str());
+		utilizationData[GPU - 1].level = memoryData[GPU - 1].used;
+		utilizationData[GPU - 1].maximum = memoryData[GPU - 1].total;
+	}
 }
 
-void UtilizationWidget::paintEvent(QPaintEvent *) {
-    QPainter p;
-    p.begin(this);
-    p.setRenderHint(QPainter::Antialiasing);
-    drawGrid(this, &p, this->GetName());
-    QMutexLocker locker(&worker->mutex);
-    drawGraph(worker, &p);
-    drawStatusObjects(statusObjectsAreas, worker->udata, &p);
-    p.end();
+void UtilizationWidget::paintEvent(QPaintEvent*)
+{
+	QPainter p;
+	p.begin(this);
+	p.setRenderHint(QPainter::Antialiasing);
+	drawGrid(this, &p, this->GetName());
+	QMutexLocker locker(&worker->mutex);
+	drawGraph(worker, &p);
+	drawStatusObjects(statusObjectsAreas, worker->utilizationData, &p);
+	p.end();
 }
 
-void UtilizationWidget::onDataUpdated() {
-    update();
+void UtilizationWidget::onDataUpdated()
+{
+	update();
 }
 
-UtilizationWidget::~UtilizationWidget() {
-    delete worker;
+UtilizationWidget::~UtilizationWidget()
+{
+	delete worker;
 }
 
-GPUUtilization::GPUUtilization() {
-    worker = new GPUUtilizationWorker;
-    setMouseTracking(true);
+GPUUtilization::GPUUtilization()
+{
+	worker = new GPUUtilizationWorker;
+	setMouseTracking(true);
 }
 
 #define area statusObjectsAreas[i]
-void GPUUtilization::mouseMoveEvent(QMouseEvent* event) {
-    for (size_t i = 0; i < statusObjectsAreas.size(); i++) {
-        if ((area.x() <= event->x()) && (area.x() + area.width() >= event->x()) &&
-            (area.y() <= event->y()) && (area.y() + area.height() >= event->y()))
-        {
-            QToolTip::showText(
-                event->globalPos(),
-                "GPU Utilization: " + QString::number(worker->udata[i].level) +
-                "\nAverage: " + QString::number(worker->udata[i].avgLevel) +
-                "\nMin: " + QString::number(worker->udata[i].minLevel) +
-                "\nMax: " + QString::number(worker->udata[i].maxLevel)
-            );
-            
-            return;
-        }
-    }
+
+void GPUUtilization::mouseMoveEvent(QMouseEvent* event)
+{
+	for (size_t i = 0; i < statusObjectsAreas.size(); i++)
+	{
+		if ((area.x() <= event->x()) && (area.x() + area.width() >= event->x()) && (area.y() <= event->y()) && (area.y() + area.height() >= event->y()))
+		{
+			QToolTip::showText(event->globalPos(), "GPU Utilization: " + QString::number(worker->utilizationData[i].level) + "\nAverage: " + QString::number(worker->utilizationData[i].avgLevel) + "\nMin: " + QString::number(worker->utilizationData[i].minLevel) + "\nMax: " + QString::number(worker->utilizationData[i].maxLevel));
+
+			return;
+		}
+	}
 }
 
-MemoryUtilization::MemoryUtilization() {
-    worker = new MemoryUtilizationWorker;
-    setMouseTracking(true);
+MemoryUtilization::MemoryUtilization()
+{
+	worker = new MemoryUtilizationWorker;
+	setMouseTracking(true);
 }
 
-void MemoryUtilization::mouseMoveEvent(QMouseEvent* event) {
-    for (size_t i = 0; i < statusObjectsAreas.size(); i++) {
-        if ((area.x() <= event->x()) && (area.x() + area.width() >= event->x()) &&
-            (area.y() <= event->y()) && (area.y() + area.height() >= event->y()))
-        {
-            QToolTip::showText(
-                event->globalPos(),
-                "Memory Utilization: " + QString::number(worker->udata[i].level) +
-                "\nAverage: " + QString::number(worker->udata[i].avgLevel) +
-                "\nMin: " + QString::number(worker->udata[i].minLevel) +
-                "\nMax: " + QString::number(worker->udata[i].maxLevel) +
-                "\nTotal: " + QString::number(((MemoryUtilizationWorker*)worker)->memoryData[i].total) + " MiB" +
-                "\nFree: " + QString::number(((MemoryUtilizationWorker*)worker)->memoryData[i].free) + " MiB" +
-                "\nUsed: " + QString::number(((MemoryUtilizationWorker*)worker)->memoryData[i].used) + " MiB"
-            );
+void MemoryUtilization::mouseMoveEvent(QMouseEvent* event)
+{
+	for (size_t i = 0; i < statusObjectsAreas.size(); i++)
+	{
+		if ((area.x() <= event->x()) && (area.x() + area.width() >= event->x()) && (area.y() <= event->y()) && (area.y() + area.height() >= event->y()))
+		{
+			QToolTip::showText(event->globalPos(),
+							   "Memory Utilization: " + QString::number(worker->utilizationData[i].level) + "\nAverage: " + QString::number(worker->utilizationData[i].avgLevel) + "\nMin: " + QString::number(worker->utilizationData[i].minLevel) + "\nMax: " + QString::number(worker->utilizationData[i].maxLevel) + "\nTotal: " + QString::number(((MemoryUtilizationWorker*)worker)->memoryData[i].total) + " MiB" + "\nFree: " + QString::number(((MemoryUtilizationWorker*)worker)->memoryData[i].free) + " MiB" + "\nUsed: " +
+							   QString::number(((MemoryUtilizationWorker*)worker)->memoryData[i].used) + " MiB");
 
-            return;
-        }
-    }
+			return;
+		}
+	}
 }
--- src/utilization.h
+++ src/utilization.h
@@ -9,89 +9,118 @@
 #include "constants.h"
 #include "worker.h"
 
-struct Point {
-    float x; // [0; 1]
-    int y; // [0; 100]
-    
-    Point(float x, int y);
+struct Point
+{
+	float x; // [0; 1]
+	int y; // [0; 100]
+
+	Point(float x, int y);
 };
 
-struct UtilizationData {
-    int level = 0; // current graph point in percents
-    int avgLevel = 0, minLevel = 0, maxLevel = 0;
+struct UtilizationData
+{
+	int level = 0; 	// current usage out of maximum
+	int avgLevel = 0, minLevel = 0, maxLevel = 0;
+	double maximum = 100;
 };
 
-struct MemoryData {
-    int total, free, used;
+struct MemoryData
+{
+	int total, free, used;
 };
 
-class UtilizationWorker : public Worker {
+class UtilizationWorker : public Worker
+{
 public:
-    std::vector<Point> *gpoints; // graph points
-    UtilizationData *udata;
+	std::vector<Point>* graphPoints; // graph points
+	UtilizationData* utilizationData;
 
-    UtilizationWorker();
-    
-    void work() override;
-    virtual void receiveData() = 0;
-    void deleteSuperfluousPoints(uint index);
-    
-    ~UtilizationWorker() override;
+	UtilizationWorker();
+
+	void work() override;
+
+	virtual void receiveData() = 0;
+
+	void deleteSuperfluousPoints(uint index);
+
+	~UtilizationWorker() override;
+
 protected:
-    long lastTime = 0;
+	long lastTime = 0;
 };
 
-class GPUUtilizationWorker : public UtilizationWorker {
+class GPUUtilizationWorker : public UtilizationWorker
+{
 public:
-    void receiveData() override;
+	void receiveData() override;
 };
 
-class MemoryUtilizationWorker : public UtilizationWorker {
+class MemoryUtilizationWorker : public UtilizationWorker
+{
 public:
-    MemoryData *memoryData;
+	MemoryData* memoryData;
 
-    MemoryUtilizationWorker();
-    ~MemoryUtilizationWorker() override;
+	MemoryUtilizationWorker();
 
-    void receiveData() override;
+	~MemoryUtilizationWorker() override;
+
+	void receiveData() override;
 };
 
-class UtilizationWidget : public QWidget {
-    Q_OBJECT
+class UtilizationWidget : public QWidget
+{
+Q_OBJECT
 public:
-    std::vector<QRect> statusObjectsAreas;
-    UtilizationWorker *worker;
+	std::vector<QRect> statusObjectsAreas;
+	UtilizationWorker* worker;
 
-	virtual const char *GetName() const {return "widget ";};
+	virtual const char* GetName() const
+	{ return "widget "; };
 
-    ~UtilizationWidget() override;
+	virtual const char* GatMax() const
+	{ return "100%"; };
 
-    void paintEvent(QPaintEvent*) override;
+	~UtilizationWidget() override;
+
+	void paintEvent(QPaintEvent*) override;
 
 public slots:
-    void onDataUpdated();
+
+	void onDataUpdated();
 };
 
-class GPUUtilization : public UtilizationWidget {
+class GPUUtilization : public UtilizationWidget
+{
 public:
-    GPUUtilization();
+	GPUUtilization();
 
-	virtual const char *GetName() const override {return "GPU use ";};
+	virtual const char* GetName() const override
+	{ return "GPU use "; };
 
-    void mouseMoveEvent(QMouseEvent *event) override;
+	void mouseMoveEvent(QMouseEvent* event) override;
 };
 
-class MemoryUtilization : public UtilizationWidget {
+class MemoryUtilization : public UtilizationWidget
+{
 public:
-    MemoryUtilization();
+	MemoryUtilization();
 
-	virtual const char *GetName() const override {return "Memory use ";};
+	virtual const char* GetName() const override
+	{ return "Memory use "; };
 
-    void mouseMoveEvent(QMouseEvent *event) override;
+	virtual const char* GatMax() const override
+	{ return max.c_str(); };
+
+	void mouseMoveEvent(QMouseEvent* event) override;
+
+private:
+	std::string max;
 };
 
-void drawGrid(QWidget *widget, QPainter *p, const char *name);
-void drawGraph(UtilizationWorker *worker, QPainter *p);
-void drawStatusObjects(std::vector<QRect> &statusObjectsAreas, UtilizationData *udata, QPainter *p);
+void drawGrid(QWidget* widget, QPainter* p, const char* name);
+
+void drawGraph(UtilizationWorker* worker, QPainter* p);
+
+void drawStatusObjects(std::vector<QRect>& statusObjectsAreas, UtilizationData* utilizationData, QPainter* p);
 
 #endif
